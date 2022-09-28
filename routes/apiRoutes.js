0826a1532ddb9ca03766ac0062a31a5fd0ad8d3b
CrisGraciano <cristobalgraciano98@gmail.com>
2022-09-28 02:53:55 -0500

    code written based on module cant get server running

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,31 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
+//var uniqid = require('uniqid');
 
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/ds.json'));
+    });
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            //id: uniqid(),
+        };
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
 
-router.get('/api/notes', (req, res) => {
-
-})
-module.exports = router; 
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let deleteNotes = db.filter(item => itemid !== req.prams.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
+    })
+};
